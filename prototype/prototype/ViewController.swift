7cb6d8a08371588c905855721fa07928b5a6f9aa
KokiSaito <koworknum@gmail.com>
2026-04-07 11:35:02 +0900

    chore: initialize project and confirm device compatibility
    - Xcode プロジェクトを新規作成（UIKit / Storyboard）
    - ターゲットOSを iOS 15.5 に設定
    - Hello World を実機シミュレートで確認

---
+++ prototype/prototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prototype
+//
+//  Created by 齋藤光貴 on 2026/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
